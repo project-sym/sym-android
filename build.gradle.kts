32f400b0f30ed4041f5d669a98d77021d13deac4
Il Harper <hi@ilharper.com>
2024-05-28 15:49:29 +0800

    chore: init spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,24 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+
+    alias(libs.plugins.spotless)
+}
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+        ktlint("1.2.1")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+
+    kotlinGradle {
+        target("**/*.kts")
+        ktlint("1.2.1")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
 }
