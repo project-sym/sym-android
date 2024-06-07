339d594ea99cae54e8a014bd088d2e18a428f90b
Il Harper <hi@ilharper.com>
2024-06-07 19:30:14 +0800

    fix(symri): element: fix `SymriElement.parse()`

--- app/src/main/java/com/ilharper/symri/element/SymriElement.kt
+++ app/src/main/java/com/ilharper/symri/element/SymriElement.kt
@@ -87,8 +87,6 @@ open class SymriElement private constructor() {
                     "&",
                 )
 
-        fun parse(source: String) {
-            Jsoup.parse(source, Parser.xmlParser()).children().map { SymriElement(it) }
-        }
+        fun parse(source: String) = Jsoup.parse(source, Parser.xmlParser()).children().map { SymriElement(it) }
     }
 }
