371d994cba02808d59fc6abd485d73ff3142a98c
Il Harper <hi@ilharper.com>
2024-06-16 00:08:04 +0800

    feat(sym): update `SettingsActivity` layout

--- app/src/main/java/com/ilharper/sym/settings/SettingsFragment.kt
+++ app/src/main/java/com/ilharper/sym/settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.ilharper.sym.settings
 
 import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference.SummaryProvider
 import androidx.preference.PreferenceFragmentCompat
 import com.ilharper.sym.R
 
@@ -10,5 +12,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         rootKey: String?,
     ) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        findPreference<EditTextPreference>(getString(R.string.sym_settings_key_server_satori_prefix))!!.summaryProvider =
+            SummaryProvider<EditTextPreference> { if (it.text.isNullOrEmpty()) "/satori" else it.text }
+
+        findPreference<EditTextPreference>(getString(R.string.sym_settings_key_server_sym_prefix))!!.summaryProvider =
+            SummaryProvider<EditTextPreference> { if (it.text.isNullOrEmpty()) "/sym" else it.text }
     }
 }
