ed10feaf5764d8ef793c67565a059633c4d6dacd
Il Harper <hi@ilharper.com>
2024-06-17 03:12:41 +0800

    feat(sym): update `SplashActivity`

--- app/src/main/java/com/ilharper/sym/splash/SplashActivity.kt
+++ app/src/main/java/com/ilharper/sym/splash/SplashActivity.kt
@@ -8,15 +8,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ilharper.sym.R
+import com.ilharper.sym.contact.ContactActivity
+import com.ilharper.sym.contactsvc.ContactService
 import com.ilharper.sym.databinding.ActivitySplashBinding
+import com.ilharper.sym.msf.Msf
 import com.ilharper.sym.settings.SettingsActivity
+import com.ilharper.sym.settings.SymSettings
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
 
+    @Inject
+    lateinit var symSettings: SymSettings
+
+    @Inject
+    lateinit var msf: Msf
+
+    @Inject
+    lateinit var contactService: ContactService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (tryEnter()) return
 
         enableEdgeToEdge()
 
@@ -30,14 +48,27 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        // binding.enter.setOnClickListener {
-        //     val intent = Intent(this, ContactActivity::class.java)
-        //     startActivity(intent)
-        // }
-
         binding.settingsButton.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        tryEnter()
+    }
+
+    private fun tryEnter(): Boolean {
+        if (!symSettings.checkLoaded()) return false
+
+        startActivity(Intent(this, ContactActivity::class.java))
+        finish()
+
+        msf.tryEnsure()
+        contactService.refresh()
+
+        return true
     }
 }
