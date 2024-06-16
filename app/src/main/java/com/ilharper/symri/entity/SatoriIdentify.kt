4498e6dae1454d288010f199a504f4968400ad47
Il Harper <hi@ilharper.com>
2024-06-17 02:56:55 +0800

    feat(symri): entity: update entities

--- app/src/main/java/com/ilharper/symri/entity/SatoriIdentify.kt
+++ app/src/main/java/com/ilharper/symri/entity/SatoriIdentify.kt
@@ -3,8 +3,8 @@ package com.ilharper.symri.entity
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-class SatoriIdentify {
-    var op: Int = SatoriOp.Identify.value
+class SatoriIdentify : SatoriEventBase {
+    override var op: Int = SatoriOp.Identify.value
 
     var token: String? = null
 
