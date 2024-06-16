0e3dd657fb0e46eaa765a558315270fc01036ec5
Il Harper <hi@ilharper.com>
2024-06-16 17:43:17 +0800

    feat(sym): apply dynamic color

--- app/src/main/java/com/ilharper/sym/app/SymApplication.kt
+++ app/src/main/java/com/ilharper/sym/app/SymApplication.kt
@@ -1,7 +1,15 @@
 package com.ilharper.sym.app
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SymApplication : Application()
+class SymApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Apply dynamic color
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
