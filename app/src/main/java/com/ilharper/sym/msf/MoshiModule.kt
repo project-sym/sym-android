263c0a4b8c17023b6428ab95420f9d9bd879dae7
Il Harper <hi@ilharper.com>
2024-06-18 01:59:23 +0800

    feat(symri): entity: update entities

--- app/src/main/java/com/ilharper/sym/msf/MoshiModule.kt
+++ app/src/main/java/com/ilharper/sym/msf/MoshiModule.kt
@@ -4,33 +4,40 @@ import com.ilharper.symri.entity.SatoriEventBase
 import com.ilharper.symri.entity.SatoriIdentify
 import com.ilharper.symri.entity.SatoriOp
 import com.ilharper.symri.entity.ext.resource.SymriContact
+import com.ilharper.symri.entity.paging.Page
+import com.ilharper.symri.entity.resource.SatoriChannel
 import com.ilharper.symri.entity.resource.SatoriChannelType
 import com.ilharper.symri.moshi.SatoriEventJsonAdapterFactory
 import com.ilharper.symri.moshi.createEnumJsonAdapter
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
+import com.squareup.moshi.addAdapter
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+@OptIn(ExperimentalStdlibApi::class)
 @Module
 @InstallIn(SingletonComponent::class)
 class MoshiModule {
     @Provides
     fun provideMoshi(): Moshi =
         Moshi.Builder()
-            .add(createEnumJsonAdapter<SatoriChannelType>())
-            .add(createEnumJsonAdapter<SatoriOp>())
+            .addAdapter(createEnumJsonAdapter<SatoriChannelType>())
+            .addAdapter(createEnumJsonAdapter<SatoriOp>())
             .add(SatoriEventJsonAdapterFactory())
             .build()
 
     @Provides
-    fun provideSatoriIdentifyJsonAdapter(moshi: Moshi): JsonAdapter<SatoriIdentify> = moshi.adapter(SatoriIdentify::class.java)
+    fun provideSatoriIdentifyJsonAdapter(moshi: Moshi) = moshi.adapter<SatoriIdentify>()
 
     @Provides
-    fun provideSatoriEventBaseJsonAdapter(moshi: Moshi): JsonAdapter<SatoriEventBase> = moshi.adapter(SatoriEventBase::class.java)
+    fun provideSatoriEventBaseJsonAdapter(moshi: Moshi) = moshi.adapter<SatoriEventBase>()
 
     @Provides
-    fun provideSymriContactJsonAdapter(moshi: Moshi): JsonAdapter<SymriContact> = moshi.adapter(SymriContact::class.java)
+    fun provideSymriContactJsonAdapter(moshi: Moshi) = moshi.adapter<SymriContact>()
+
+    @Provides
+    fun providePageSatoriChannelJsonAdapter(moshi: Moshi) = moshi.adapter<Page<SatoriChannel>>()
 }
