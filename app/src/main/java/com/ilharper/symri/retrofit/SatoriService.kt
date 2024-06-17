5f98cc3be832c4974e9aa1143adce652ff6d4bd4
Il Harper <hi@ilharper.com>
2024-06-18 02:43:11 +0800

    feat(symri): entity: update entities

--- app/src/main/java/com/ilharper/symri/retrofit/SatoriService.kt
+++ app/src/main/java/com/ilharper/symri/retrofit/SatoriService.kt
@@ -1,4 +1,18 @@
 package com.ilharper.symri.retrofit
 
+import com.ilharper.symri.entity.paging.Page
+import com.ilharper.symri.entity.payload.ListChannelPayload
+import com.ilharper.symri.entity.resource.SatoriChannel
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface SatoriService {
+    @POST("channel.list")
+    fun listChannel(
+        @Header("X-Platform") platform: String,
+        @Header("X-Self-ID") loginId: String,
+        @Body payload: ListChannelPayload,
+    ): Call<Page<SatoriChannel>>
 }
