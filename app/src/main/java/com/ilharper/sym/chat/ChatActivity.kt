96b6c0aaecc18fb6bfe8e6220b3b50812636d96a
Il Harper <hi@ilharper.com>
2024-06-18 19:01:03 +0800

    feat(sym): update `ChatActivity` layout

--- app/src/main/java/com/ilharper/sym/chat/ChatActivity.kt
+++ app/src/main/java/com/ilharper/sym/chat/ChatActivity.kt
@@ -1,11 +1,14 @@
 package com.ilharper.sym.chat
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updatePadding
 import androidx.recyclerview.widget.LinearLayoutManager
 import coil.ImageLoader
+import com.ilharper.sym.R
 import com.ilharper.sym.databinding.ActivityChatBinding
 import com.ilharper.symri.entity.ext.resource.SymriContact
 import com.squareup.moshi.JsonAdapter
@@ -67,5 +70,38 @@ class ChatActivity : AppCompatActivity() {
             )
         binding.listView.adapter = chatAdapter
         vm.messages.observe(this) { binding.listView.scrollToPosition(0) }
+
+        binding.appBarContainer.addOnLayoutChangeListener(updateListViewPaddingTop)
+        binding.bottomAppBar.addOnLayoutChangeListener(updateListViewPaddingBottom)
     }
+
+    private val updateListViewPaddingTop =
+        View.OnLayoutChangeListener {
+                v: View?,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+            ->
+            binding.listView.updatePadding(top = v!!.height + resources.getDimensionPixelOffset(R.dimen.activity_chat_listview_padding))
+        }
+
+    private val updateListViewPaddingBottom =
+        View.OnLayoutChangeListener {
+                v: View?,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+                _: Int,
+            ->
+            binding.listView.updatePadding(bottom = v!!.height + resources.getDimensionPixelOffset(R.dimen.activity_chat_listview_padding))
+        }
 }
